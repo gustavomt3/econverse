4b790696e4af9fbb0a3f0d10a3a20ca9ca97cd34
Gustavo Melo <contato@pepinosolution.com>
2023-03-08 09:15:42 -0300

    feat: ajuste funcional no componente Slider

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -8,6 +8,27 @@ import Filter from '../Filter/Filter';
 import SimpleSlider from '../Slider/SimpleSlider';
 //Styles
 import styles from './ProductList.module.scss';
+//Imagens
+import ArrowNext from '../../Assests/ArrowNext';
+import ArrowPrev from '../../Assests/ArrowPrev';
+
+function SampleNextArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowNext />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowPrev />
+    </div>
+  );
+}
 
 const ProductList = ({ title, option }) => {
   //Context
@@ -33,7 +54,57 @@ const ProductList = ({ title, option }) => {
         </div>
       )}
       {!loading && data && (
-        <SimpleSlider>
+        <SimpleSlider
+          settings={{
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: true,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />,
+            className: `${styles.slickSlide}`,
+            responsive: [
+              {
+                breakpoint: 590,
+                settings: {
+                  slidesToShow: 1,
+                  arrows: false,
+                  dots: true,
+                  className: `${styles.slickSlide}`,
+                },
+              },
+              {
+                breakpoint: 850,
+                settings: {
+                  slidesToShow: 2,
+                  arrows: false,
+                  dots: true,
+                  className: `${styles.slickSlide}`,
+                },
+              },
+              {
+                breakpoint: 1155,
+                settings: {
+                  slidesToShow: 3,
+                  arrows: false,
+                  dots: true,
+                  className: `${styles.slickSlide}`,
+                },
+              },
+              {
+                breakpoint: 1410,
+                settings: {
+                  slidesToShow: 4,
+                  arrows: false,
+                  dots: true,
+                  className: `${styles.slickSlide}`,
+                },
+              },
+            ],
+          }}
+        >
           {data.map((data, index) => {
             return <Card key={index} data={data} setModalCard={setModalCard} />;
           })}
--- src/Components/Slider/SimpleSlider.jsx
+++ src/Components/Slider/SimpleSlider.jsx
@@ -4,82 +4,10 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-//Styles
-import styles from './SimpleSlider.module.scss';
-//Imagens
-import ArrowNext from '../../Assests/ArrowNext';
-import ArrowPrev from '../../Assests/ArrowPrev';
-
-function SampleNextArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      <ArrowNext />
-    </div>
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      <ArrowPrev />
-    </div>
-  );
-}
 
 class SimpleSlider extends React.Component {
   render() {
-    const { children } = this.props;
-    const settings = {
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: true,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />,
-      className: `${styles.slickSlide}`,
-      responsive: [
-        {
-          breakpoint: 590,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-            dots: true,
-            className: `${styles.slickSlide}`,
-          },
-        },
-        {
-          breakpoint: 850,
-          settings: {
-            slidesToShow: 2,
-            arrows: false,
-            dots: true,
-            className: `${styles.slickSlide}`,
-          },
-        },
-        {
-          breakpoint: 1155,
-          settings: {
-            slidesToShow: 3,
-            arrows: false,
-            dots: true,
-            className: `${styles.slickSlide}`,
-          },
-        },
-        {
-          breakpoint: 1410,
-          settings: {
-            slidesToShow: 4,
-            arrows: false,
-            dots: true,
-            className: `${styles.slickSlide}`,
-          },
-        },
-      ],
-    };
+    const { children, settings } = this.props;
     return <Slider {...settings}>{children}</Slider>;
   }
 }
